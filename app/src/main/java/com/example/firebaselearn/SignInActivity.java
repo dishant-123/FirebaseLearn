5df131bd0274b427efe90ceaf2ab8a3459501503
Dishant <dishantdua8956@gmail.com>
2021-02-23 15:47:22 +0530

    lecture-0 source code

---
+++ app/src/main/java/com/example/firebaselearn/SignInActivity.java
@@ -0,0 +1,87 @@
+package com.example.firebaselearn;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.firebaselearn.databinding.ActivitySignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ActivitySignInBinding binding;
+    private ProgressDialog progressDialog;
+    public SignInActivity(){}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        initListener();
+        setContentView(binding.getRoot());
+    }
+
+    private void initListener() {
+        binding.signInButton.setOnClickListener(this);
+        binding.signUpButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.signInButton:
+                if(isDataValid()) {
+                    doSignInWithEmailAndPassword();
+                }
+                break;
+            case R.id.signUpButton:
+                Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    private boolean isDataValid() {
+        if(binding.passwordTxt.getText().toString().isEmpty()){
+            binding.passwordTxt.requestFocus();
+            Toast.makeText(this, "Please provide Password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(binding.emailTxt.getText().toString().isEmpty()){
+            binding.emailTxt.requestFocus();
+            Toast.makeText(this, "Please provide email", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void doSignInWithEmailAndPassword() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please Wait..");
+        progressDialog.setMessage("Loging to your account");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                binding.emailTxt.getText().toString().trim(),
+                binding.passwordTxt.getText().toString().trim()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                if(task.isSuccessful()){
+                    Toast.makeText(SignInActivity.this, "User SignIn Successfully", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(SignInActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/firebaselearn/SignUpActivity.java
@@ -0,0 +1,92 @@
+package com.example.firebaselearn;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.firebaselearn.databinding.ActivitySignUpBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ActivitySignUpBinding binding;
+    private ProgressDialog progressDialog;
+    public SignUpActivity(){}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        initListener();
+        setContentView(binding.getRoot());
+    }
+
+    private void initListener() {
+        binding.signUpButton.setOnClickListener(this);
+        binding.signInButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.signUpButton:
+                if(isDataValid()) {
+                    signUpWithEmailAndPassword();
+                }
+                break;
+            case R.id.signInButton:
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+
+    private boolean isDataValid() {
+        if(binding.nameTxt.getText().toString().isEmpty()){
+            binding.nameTxt.requestFocus();
+            Toast.makeText(this, "Please provide name", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if(binding.passwordTxt.getText().toString().isEmpty()){
+            binding.passwordTxt.requestFocus();
+            Toast.makeText(this, "Please provide Password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else if(binding.emailTxt.getText().toString().isEmpty()){
+            binding.emailTxt.requestFocus();
+            Toast.makeText(this, "Please provide email", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void signUpWithEmailAndPassword() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please Wait..");
+        progressDialog.setMessage("We are creating your account");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                binding.emailTxt.getText().toString().trim(),
+                binding.passwordTxt.getText().toString().trim()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                if(task.isSuccessful()){
+                    Toast.makeText(SignUpActivity.this, "User SignUp Successfully", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(SignUpActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
